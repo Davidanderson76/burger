17c3f46af4bd632b9515fab73a7af5711b717f20
David Anderson <david.anderson1993@gmail.com>
2020-10-29 17:30:54 -0500

    laying out the file system and have schema written

---
+++ server.js
@@ -0,0 +1,34 @@
+const exphbs = require("express-handlebars");
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "quotes_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
